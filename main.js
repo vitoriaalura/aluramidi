c08e9700eb3f16ef065ab30e46428c4a8da1c765
vitoriaalura <117860645+vitoriaalura@users.noreply.github.com>
2023-10-17 21:53:44 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,2 +1,3 @@
 alert('va estudar nene');
-<button onclik="alert('Clap')" class="tecla tecla_clap">Clap</button>
+<button onclik="alert('Clap')" class="tecla tecla_clap">Clap</button>;
+<audio controls="" src="sounds/keyq.wav" id="som_tecla_pom"></audio>play
